8e969c339ddea564150c086e800b91a7b9c40651
gggeek <giunta.gaetano@gmail.com>
2022-02-27 15:02:36 +0000

    make all shell scripts executable

--- bin/setup/php.sh
+++ bin/setup/php.sh
@@ -8,9 +8,11 @@ if [ "${PHPVER}" != "${PHP_VERSION}" ]; then
     cd "$(dirname -- $(dirname -- $(dirname -- ${BASH_SOURCE[0]})))"
 
     if [ -f ./docker/images/ez/root/build/getphp.sh ]; then
+        chmod 755 ./docker/images/ez/root/build/getphp.sh
         ./docker/images/ez/root/build/getphp.sh
     else
         if [ -f /home/test/teststack/docker/images/ez/root/build/getphp.sh ]; then
+            chmod 755 /home/test/teststack/docker/images/ez/root/build/getphp.sh
             /home/test/teststack/docker/images/ez/root/build/getphp.sh
         else
             echo "Error: php version ${PHPVER} does not match required ${PHP_VERSION} and can not find script to set up php" >&2
