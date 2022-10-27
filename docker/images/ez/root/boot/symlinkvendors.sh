33cd78a05ac6294c0fee081df29b63b4f443a01e
gggeek <giunta.gaetano@gmail.com>
2022-10-27 08:43:06 +0000

    fix for renaming /home/test/bundle to home/test/workspace

--- docker/images/ez/root/boot/symlinkvendors.sh
+++ docker/images/ez/root/boot/symlinkvendors.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-ORIGPASSWD=$(cat /etc/passwd | grep '^test:')
+# @todo grab the workspace dir from cli options
+
+CONTAINER_USER=test
+ORIGPASSWD=$(cat /etc/passwd | grep "^${CONTAINER_USER}:")
 CONTAINER_USER_HOME=$(echo "$ORIGPASSWD" | cut -f6 -d:)
+WORKSPACE_DIR="${CONTAINER_USER_HOME}/workspace"
 
 # WAS: we hash the name of the vendor folder based on packages to install. This allows quick swaps of vendors
 #if [ -z "${TESTSTACK_VENDOR_DIR}" ]; then
@@ -13,19 +17,19 @@ CONTAINER_USER_HOME=$(echo "$ORIGPASSWD" | cut -f6 -d:)
 #fi
 TESTSTACK_VENDOR_DIR="vendor_${COMPOSE_PROJECT_NAME}"
 
-# @todo we assume that /home/test/workspace/vendor is never a file...
+# @todo we assume that /${WORKSPACE_DIR}/vendor is never a file...
 
-if [ -d "${CONTAINER_USER_HOME}/workspace/vendor" -a ! -L "${CONTAINER_USER_HOME}/workspace/vendor" ]; then
+if [ -d "${WORKSPACE_DIR}/vendor" -a ! -L "${WORKSPACE_DIR}/vendor" ]; then
     printf "\n\e[33mWARNING:\e[0m vendor folder is not a symlink\n\n"
 fi
 
-if [ -L "${CONTAINER_USER_HOME}/workspace/vendor" -o ! -d "${CONTAINER_USER_HOME}/workspace/vendor" ]; then
+if [ -L "${WORKSPACE_DIR}/vendor" -o ! -d "${WORKSPACE_DIR}/vendor" ]; then
     echo "[$(date)] Setting up vendor folder as symlink to ${TESTSTACK_VENDOR_DIR}..."
 
-    if [ ! -d "${CONTAINER_USER_HOME}/workspace/${TESTSTACK_VENDOR_DIR}" ]; then
-        mkdir "${CONTAINER_USER_HOME}/workspace/${TESTSTACK_VENDOR_DIR}"
+    if [ ! -d "${WORKSPACE_DIR}/${TESTSTACK_VENDOR_DIR}" ]; then
+        mkdir "${WORKSPACE_DIR}/${TESTSTACK_VENDOR_DIR}"
     fi
-    chown -R test:test "${CONTAINER_USER_HOME}/workspace/${TESTSTACK_VENDOR_DIR}"
+    chown -R "${CONTAINER_USER}:${CONTAINER_USER}" "${WORKSPACE_DIR}/${TESTSTACK_VENDOR_DIR}"
 
     # The double-symlink craze makes it possible to have the 'vendor' symlink on the host disk (mounted as volume),
     # while allowing each container to have it point to a different target 'real' vendor dir which is also on the
@@ -35,19 +39,19 @@ if [ -L "${CONTAINER_USER_HOME}/workspace/vendor" -o ! -d "${CONTAINER_USER_HOME
     if [ -L "${CONTAINER_USER_HOME}/local_vendor" ]; then
         rm "${CONTAINER_USER_HOME}/local_vendor"
     fi
-    ln -s "${CONTAINER_USER_HOME}/bundle/${TESTSTACK_VENDOR_DIR}" "${CONTAINER_USER_HOME}/local_vendor"
+    ln -s "${WORKSPACE_DIR}/${TESTSTACK_VENDOR_DIR}" "${CONTAINER_USER_HOME}/local_vendor"
 
-    if [ -L "${CONTAINER_USER_HOME}/bundle/vendor" ]; then
-        TARGET=$(readlink -f "${CONTAINER_USER_HOME}/bundle/vendor")
-        if [ "${TARGET}" != "${CONTAINER_USER_HOME}/bundle/${TESTSTACK_VENDOR_DIR}" ]; then
-            echo "[$(date)] Fixing vendor folder symlink from ${TARGET} to ${CONTAINER_USER_HOME}/bundle/${TESTSTACK_VENDOR_DIR}..."
-            rm "${CONTAINER_USER_HOME}/bundle/vendor"
-            ln -s "${CONTAINER_USER_HOME}/local_vendor" "${CONTAINER_USER_HOME}/bundle/vendor"
+    if [ -L "${WORKSPACE_DIR}/vendor" ]; then
+        TARGET=$(readlink -f "${WORKSPACE_DIR}/vendor")
+        if [ "${TARGET}" != "${WORKSPACE_DIR}/${TESTSTACK_VENDOR_DIR}" ]; then
+            echo "[$(date)] Fixing vendor folder symlink from ${TARGET} to ${WORKSPACE_DIR}/${TESTSTACK_VENDOR_DIR}..."
+            rm "${WORKSPACE_DIR}/vendor"
+            ln -s "${CONTAINER_USER_HOME}/local_vendor" "${WORKSPACE_DIR}/vendor"
             if [ -f "${CONTAINER_USER_HOME}/setup_ok" ]; then rm "${CONTAINER_USER_HOME}/setup_ok"; fi
         fi
     else
         echo "[$(date)] Creating vendor folder symlink to ${TESTSTACK_VENDOR_DIR}..."
-        ln -s "${CONTAINER_USER_HOME}/local_vendor" "${CONTAINER_USER_HOME}/bundle/vendor"
+        ln -s "${CONTAINER_USER_HOME}/local_vendor" "${WORKSPACE_DIR}/vendor"
         if [ -f "${CONTAINER_USER_HOME}/setup_ok" ]; then rm "${CONTAINER_USER_HOME}/setup_ok"; fi
     fi
 fi
